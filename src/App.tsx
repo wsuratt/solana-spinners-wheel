0604d3d3b4ed3beeb3729cfe417ce090ffb2ff06
William Suratt <wsuratt@comcast.net>
2022-05-20 19:55:17 -0400

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Typography } from '@material-ui/core' 
 import './App.css';
+import Wheel from './components/wheel';
+import ButtonAppBar from './components/ButtonAppBar';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import placeholderArt from './assets/placeholderArt.jpg';
+import MobilePage from './MobilePage'
 
 function App() {
+  const places = [
+    'Pizzas', 'Sandwiches', 'Salads', 'Soup', 'Japanese food', 'Pastas',
+    'Pizzas', 'Sandwiches', 'Salads', 'Soup', 'Japanese food', 'Pastas'
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='mobilePage'>
+        <MobilePage/>
+      </div>
+      <div className='placeholder'>
+        <ButtonAppBar/>
+        <div className="content">
+          <Typography variant="h1">
+            NFT WHEEL
+          </Typography>
+          <img src={placeholderArt} alt="placeholderArt"/>
+        </div>
+        <Wheel items={places} />
+      </div>
     </div>
   );
 }
---
+++ src/MobilePage.tsx
@@ -0,0 +1,11 @@
+import Typography from '@mui/material/Typography';
+
+function MobilePage() {
+    return (  
+        <Typography sx={{ color: 'white', marginTop: '25px' }}>
+        Not available on mobile
+        </Typography>
+    )
+}
+
+export default MobilePage
